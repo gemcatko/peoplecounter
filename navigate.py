1fb30ff529cad712b4f511feee4d4e88439d66c1
gem <sol>
2020-08-15 12:43:51 +0200

    added stop and go back added distance for every object refactored navigate left right id inplemneted again

--- navigate.py
+++ navigate.py
@@ -3,11 +3,13 @@ from dev_env_vars import *
 from datetime import datetime
 
 def navigate(distance_results):
-    for distance,cat, score, bounds in distance_results:
+    for distance,id,cat, score, bounds in distance_results:
         try:
             if "person" == bytes.decode(cat):
                 if not to_close(distance):
                     cur_time = str(datetime.now().time()) + " GO"
+                    detection = [distance,cat, score, bounds]
+                    rotate_to_target(detection)
                     logging.info(cur_time)
 
             if "cell phone" == bytes.decode(cat):
@@ -25,3 +27,24 @@ def to_close(distance):
         logging.info(cur_time)
         return True
 
+def rotate_to_target (detection):
+
+    x, y, w, h = detection[3][0], \
+                         detection[3][1], \
+                         detection[3][2], \
+                         detection[3][3]
+    middleX= Xresolution / 2
+    middleY= Yresolution / 2
+    darknetvscameraresolutionx = (Xresolution / network_width)
+    darknetvscameraresolutiony = (Yresolution / network_heigth)
+    x = x * darknetvscameraresolutionx
+    y = y * darknetvscameraresolutiony
+    w = w * darknetvscameraresolutionx
+    h = h * darknetvscameraresolutiony
+    try:
+        if x < middleX:
+             logging.info("Rotate to left")
+        if x > middleX:
+            logging.info("Rotate to right")
+    except Exception as e:
+        print(e)
