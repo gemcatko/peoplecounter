776ea736feb8748f43791aceab902373205765c3
gem <sol>
2020-08-16 15:37:51 +0200

    added stop and go back added distance for every object refactored navigate left right id inplemneted again  dokonci map_visualization there is bug when drawing

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
+import multiprocessing
 import os
-import cv2
-import time
 import sys
-from datetime import datetime
+import time
+
 # insert at 1, 0 is the script path (or '' in REPL)
 #sys.path.insert(1, '/home/automateit/Projects/darknet-alexeyAB/darknet')
 #sys.path.insert(1, '/home/ce/Projects/darknet_alex_darknet')
 sys.path.insert(1, '/home/solcanma/darknet')
 import darknet
-from multiprocessing import Process, Value, Array, Manager
+from multiprocessing import Process, Manager
 from multiprocessing import shared_memory
-import numpy as np
-from dev_env_vars import *
+
 manager = Manager()
 manager_detections = manager.list()
 from pyimagesearch.centroidtracker import *
-import logging
 from distance import *
 from navigate import *
+from map import *
 
 #from pyimagesearch.centroidtracker import *
 
-
-
-shm = shared_memory.SharedMemory(create=True, size=6520800, name='psm_c013ddb1')
-#shm_image = np.ndarray((network_width, network_heigth, 3), dtype=np.uint8, buffer=shm.buf)
+shm = shared_memory.SharedMemory(create=True, size=6520800, name='psm_c013ddb5')
 shm_image = np.ndarray((Yresolution, Xresolution, 3), dtype=np.uint8, buffer=shm.buf)
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s', )
-
 # import the necessary packages
-from collections import OrderedDict
 
 import numpy as np
-from scipy.spatial import distance as dist
 
 
 def convertBack(x, y, w, h):
@@ -125,7 +118,7 @@ def YOLO():
     print(cap.get(cv2.CAP_PROP_FPS))
     print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    #cap = cv2.VideoCapture(video_filename_path)
+
     cap.set(3, Xresolution)
     cap.set(4, Yresolution)
     out = cv2.VideoWriter(
@@ -136,6 +129,8 @@ def YOLO():
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                        darknet.network_height(netMain), 3)
+    # definition how much frames when object is marked as dissapear
+    ct = CentroidTracker(maxDisappeared=20)
 
     while True:
         prev_time = time.time()
@@ -150,19 +145,16 @@ def YOLO():
         darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
         del manager_detections[:]  # need to be cleared every iterration
         detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=detection_treshold)
-        #detections = update_resutls_for_id(detections)
-        manager_detections.append(detections)
+        ct_objects = ct.update(convert_bounding_boxes_form_Yolo_Centroid_format(detections))
+        id_detections = update_resutls_for_id(detections,ct_objects)
         image = cvDrawBoxes(detections, frame_rgb)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-
-        distance_results=update_resutls_for_distance(detections)
-        draw_distance_next_to_bb_box(image,distance_results)
-        navigate(distance_results)
-
-
-
-
+        dist_id_detections=update_resutls_for_distance(id_detections)
+        draw_distance_next_to_bb_box(image,dist_id_detections)
+        draw_id(image,dist_id_detections,)
+        navigate(dist_id_detections)
+        manager_detections.append(dist_id_detections)
 
         shm_image[:] = image[:]  # copy image to shared memory as array because we would like to share with other proces
         end_time = time.time()
@@ -175,6 +167,8 @@ def YOLO():
             break
     cap.release()
     out.release()
+    shm.close()
+    shm.unlink()
 
 def convert_bounding_boxes_form_Yolo_Centroid_format(results):
     # clean rect so it is clean an can be filled with new detection from frame\
@@ -184,12 +178,12 @@ def convert_bounding_boxes_form_Yolo_Centroid_format(results):
 
     # if len(results) <= 1: # check if array is not empty, for prevention of crashing in later stage
     #    return []
-    print("resultsinside convert bound..",results)
+    #print("resultsinside convert bound..",results)
     try:
         for cat, score, bounds in results:  # unpacking
-            print("cat, score, bounds ", cat, score, bounds )
+            #print("cat, score, bounds ", cat, score, bounds )
             x, y, w, h = bounds
-            print( "x, y, w, h", x, y, w, h)
+            #print( "x, y, w, h", x, y, w, h)
             """
             convert from yolo format to cetroid format
             Yolo output:
@@ -248,18 +242,10 @@ def rotate_by_angel_and_delay(frame,angel, delay):
     time.sleep(delay)
     return rotated
 
-def show_fps(start_time, end_time, name_of_frame):
-    duration_of_loop = end_time - start_time
-    FPS = round(1 / duration_of_loop, 1)
-    cv2.putText(name_of_frame, str(FPS), (int(Xres - 80), int(Yres - 40)), cv2.FONT_HERSHEY_COMPLEX, 1,
-                (255, 100, 255))
-    # print(FPS)
-    return FPS
-
 class YObject:
     # use for creating objects from Yolo.
     # def __init__(self, centroid_id, category, score, bounds):
-    def __init__(self, id, category, score, bounds):
+    def __init__(self,distance, id, category, score, bounds):
         # copy paste functionality of  detect_object_4_c
         self.id = id
         self.category = category
@@ -269,7 +255,7 @@ class YObject:
         self.is_detected_by_detector = True
         self.ignore = False
         self.is_picture_saved = False
-        self.distance = None
+        self.distance = distance
         # self.ready_for_blink_start = False
         # self.ready_for_blink_end = False
         global frame
@@ -332,31 +318,31 @@ class YObject:
             save_picture_to_file(file_name)
         self.is_picture_saved = True
 
-def update_resutls_for_id(results, ct_objects):
+def update_resutls_for_id(results,ct_objects):
     """
     loop over the tracked objects from Yolo34
     Reconstruct Yolo34 results with object id (data from centroid tracker) an put object ID to idresults list, like :
     class 'list'>[(b'person', 0.9972826838493347, (646.4600219726562, 442.1628112792969, 1113.6322021484375, 609.4992065429688)), (b'bottle', 0.5920438170433044, (315.3851318359375, 251.22744750976562, 298.9032287597656, 215.8708953857422))]
     class 'list'>[(1, b'person', 0.9972826838493347, (646.4600219726562, 442.1628112792969, 1113.6322021484375, 609.4992065429688)), (4, b'bottle', 0.5920438170433044, (315.3851318359375, 251.22744750976562, 298.9032287597656, 215.8708953857422))]
-    :param results from Darknet, ct_objects:
+    :param results from Yolo34:
     :return:idresults
     """
     idresults = []
-    try:
-        for cat, score, bounds in results:
-            x, y, w, h = bounds
-            # loop over the tracked objects from Centroid
-            for (objectID, centroid) in ct_objects.items():
-                # put centroid coordinates to cX and Cy variables
-                cX, cY = centroid[0], centroid[1]
-                # there is difference between yolo34 centroids and centroids calculated by centroid tracker,Centroid closer then 2 pixel are considired to matcg  TODO find where?
-                if abs(cX - int(x)) <= 2 and abs(cY - int(y)) <= 2:
-                    # reconstruct detection list as from yolo34 including ID from centroid
-                    idresult = objectID, cat, score, bounds
-                    idresults.append(idresult)
-        return idresults
-    except:
-        return idresults
+    for cat, score, bounds in results:
+        x, y, w, h = bounds
+        # chyba_one_mark_small("cell phone", cat, score, x, y, w, h, )
+        # loop over the tracked objects from Centroid
+        #print(ct_objects)
+        for ct_object in ct_objects.items():
+            # put centroid id and coordinates to cX and Cy variables
+            id, coordinates = ct_object[0], ct_object[1]
+            cX, cY = coordinates[0], coordinates[1]
+            # there is difference between yolo34 centroids and centroids calculated by centroid tracker,Centroid closer then 2 pixel are considired to matcg  TODO find where?
+            if abs(cX - int(x)) <= 2 and abs(cY - int(y)) <= 2:
+                # reconstruct detection list as from yolo34 including ID from centroid
+                idresult = id, cat, score, bounds
+                idresults.append(idresult)
+    return idresults
 
 
 def show_fps(start_time, end_time, name_of_frame):
@@ -378,66 +364,47 @@ def is_Yobject_to_big(bounds):
     return False
 
 
-def second_visualization(net_width, net_heigth):
-    existing_shm = shared_memory.SharedMemory(name='psm_c013ddb7')
-    image = np.ndarray((net_width, net_heigth, 3), dtype=np.uint8, buffer=existing_shm.buf)
-    ct = CentroidTracker(maxDisappeared=2000)
-    which_id_to_delete = 0  # is used for object deletion start
+def second_visualization(Xresolution, Yresolution):
+    #existing_shm = shared_memory.SharedMemory(name='psm_c013ddb7', create=True,size=net_width*net_heigth*3)
+    #image = np.ndarray((net_width, net_heigth, 3), dtype=np.uint8, buffer=existing_shm.buf)
+
     start_time = time.time()  # for FPS counting
     while True:
-        frame = image
+        frame = shm_image
         # get data from shared memory
         results = manager_detections
         results = unpack_results(results)
-        # print("Results from darkent:",results)
-        # genreate IDs for for results from Darknet
-        ct_objects = ct.update(convert_bounding_boxes_form_Yolo_Centroid_format(results))
-        idresults = update_resutls_for_id(results, ct_objects)
-        # print("Resultswith ID", idresults)
-        for id, category, score, bounds in idresults:
-            try:  #
-                # if Yobjekt with specific id already exists, update it
-                # TODO # to je mozno chyba,co sa stane z objektami ktorych id je este na zobrazene ale nuz je objekt dissapeared
-                if objekty[id].id == id:  # and objekty[id].category == category.decode("utf-8"):
-                    # if objekty[id].category == category.decode("utf-8"): #- If you enable you need to take care of umached objects which are deteceted
-                    if not objekty[id].is_big:
-                        objekty[id].category = category.decode("utf-8")
-                        objekty[id].score = score
-                        objekty[id].bounds = bounds
+        if results :
+            #print("results from second loop", results)
 
-                        objekty[id].is_detected_by_detector = True
-                        if is_Yobject_to_big(bounds):           # if objects is across whole camera view it need to go to trail visulaization so it can be marked. If not done centroid tracker whould hold it on  visible screen and it would be marked as very smal object when leaving camera view
-                            objekty[id].is_big = True
+            for dist, id, category, score, bounds in results:
+                try:
+                    # if Yobjekt with specific id already exists, update it
+                    # TODO # to je mozno chyba,co sa stane z objektami ktorych id je este na zobrazene ale nuz je objekt dissapeared
+                    if objekty[id].id == id: #and objekty[id].category == category.decode("utf-8"):
+                        if objekty[id].category == category.decode("utf-8"):
+                            objekty[id].category = category.decode("utf-8")
+                            objekty[id].score = score
+                            objekty[id].bounds = bounds
+                            objekty[id].is_detected_by_detector = True
+                except Exception as e:
+                    print(e)
+                    # create new object if not existing
+                    objekty[id] = YObject(dist ,id, category.decode("utf-8"), score, bounds)
+            update_objekty_if_not_detected(objekty)
+            #print(len(objekty))
+            #print(objekty)
+            if len(objekty) > 25: # max number of object which draw on trail
+                del objekty[number_of_deleted_objects]
+                number_of_deleted_objects = number_of_deleted_objects + 1
 
-            except:
-                # create new object if not existing
-                objekty[id] = YObject(id, category.decode("utf-8"), score, bounds)
-        if len(objekty) > max_Yobject:  # max number of object which to keep
-            del objekty[which_id_to_delete]
-            which_id_to_delete = which_id_to_delete + 1
-        count_visible_objekt("person",objekty)
-        cv2.putText(frame, "Number of person on screen:" + str(count_visible_objekt("person",objekty)), (20, 20), cv2.FONT_HERSHEY_COMPLEX,font_size,azzure)
-        print(count_visible_objekt("person",objekty))
-        for id in objekty:
-            objekty[id].draw_object_bb(frame, idresults)
-            #objekty[id].draw_category(frame, idresults)
-            #objekty[id].draw_object_score(frame, idresults)
-            objekty[id].draw_object_id(frame, idresults)
-            #objekty[id].draw_object_position_on_trail(frame, idresults)
-            # objekty[id].save_picure_of_every_detected_object("detected_objects")
-        update_objekty_if_not_detected(objekty, idresults)
-        """
-        try:
-            check_on_vysialization(draw_trail_visualization(objekty, s_distance), objekty, s_distance)
-        except Exception as ex:
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-        """
-        end_time = time.time()
-        #show_fps(start_time, end_time, frame)
-        start_time = time.time()
-        cv2.imshow('second_visualization', frame)
+            #do stuff here
+            map_visualization(objekty)
+            cv2.imshow('second_visualization', frame)
+
+
+
+
         k = cv2.waitKey(1)
         if k == 0xFF & ord("q"):
             break
@@ -448,20 +415,6 @@ def count_visible_objekt(object_to_count, objekty):
         if objekty[id].is_detected_by_detector == True:
           count_of_objects += 1
     return count_of_objects
-
-
-
-def update_objekty_if_not_detected(objekty, idresults):
-    """
-    :param objekty:
-    Is updating all objects store in objekty if not in in idresults list from detector
-    :return:
-    """
-    for id in objekty:
-        # id in (item for sublist in idresults for item in sublist) is returning True or False good explanation is https://www.geeksforgeeks.org/python-check-if-element-exists-in-list-of-lists/
-        if not id in (item for sublist in idresults for item in sublist):
-            objekty[id].is_detected_by_detector = False
-
 
 
 def calculate_relative_coordinates(x, y, w, h):
@@ -480,10 +433,43 @@ def calculate_relative_coordinates(x, y, w, h):
     area_rel = w_rel * h_rel
     return x_rel, y_rel, w_rel, h_rel, area_rel
 
+def draw_id(img,dist_id_detectios):
+    #print("Distance",distance_results)
+    for detection in dist_id_detectios:
+        x, y, w, h = detection[4][0], \
+                     detection[4][1], \
+                     detection[4][2], \
+                     detection[4][3]
+        darknetvscameraresolutionx = (Xresolution / network_width)
+        darknetvscameraresolutiony = (Yresolution / network_heigth)
+        x = x * darknetvscameraresolutionx
+        y = y * darknetvscameraresolutiony
+        w = w * darknetvscameraresolutionx
+        h = h * darknetvscameraresolutiony
+        cv2.putText(img, "ID " + str(int(detection[1] )), (int(x), int(y)-20), cv2.FONT_HERSHEY_COMPLEX,
+                    font_size,
+                    azzure)
+    return img
 
-#second_visualization_proc = Process(target=second_visualization, args=(network_width, network_heigth))
-#second_visualization_proc.daemon = True
+def update_objekty_if_not_detected(objekty):
+    """
+    :param objekty:
+    Is updating all objects store in objekty if not in in idresults list from detector
+    :return:
+    """
+    for id in objekty:
+        # id in (item for sublist in idresults for item in sublist) is returning True or False good explanation is https://www.geeksforgeeks.org/python-check-if-element-exists-in-list-of-lists/
+        if not id in (item for sublist in idresults for item in sublist):
+            objekty[id].is_detected_by_detector = False
+            #objekty[id].position_on_trail = s_distance.value
+
+
+second_visualization_proc = multiprocessing.Process(target=second_visualization, args=(Xresolution,Yresolution,))
+second_visualization_proc.daemon = True
+
+
 
 if __name__ == "__main__":
-    #second_visualization_proc.start()
+
+    second_visualization_proc.start()
     YOLO()
